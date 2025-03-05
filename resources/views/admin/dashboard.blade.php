fbc823f0dd581a0a7d9910cab10a522c8f5d5aed
SacSLAF <kalpanairosh@gmail.com>
2025-03-06 00:08:51 +0530

    3rd dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,19 +10,19 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h4>Admin Dashboard</h4>
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('admin.logout') }}">
+                        @csrf
+                        <x-dropdown-link :href="route('logout')"
+                            onclick="event.preventDefault();
+            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>
                     {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
     </div>
-    <!-- Authentication -->
-    <form method="POST" action="{{ route('admin.logout') }}">
-        @csrf
 
-        <x-dropdown-link :href="route('logout')"
-            onclick="event.preventDefault();
-            this.closest('form').submit();">
-            {{ __('Log Out') }}
-        </x-dropdown-link>
-    </form>
 </x-app-layout>
